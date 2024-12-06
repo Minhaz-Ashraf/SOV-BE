b96673a0ffeae2036180ddb3724a200b2545625d
Minhaz Ashraf <minhazashraf590@gmail.com>
2024-12-06 14:27:19 +0530

    changes

--- models/agent.model.js
+++ models/agent.model.js
@@ -60,7 +60,6 @@ const agentSchema = new Schema(
         },
         phone: {
           type: String,
-          required: [true, "Primary Contact Person Phone is required"],
         },
       },
       referralSource: {
